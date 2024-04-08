5ed0a4621c47dcc0835870c8bbdd0f89400ed620
Kasper Bergman Hemdator <kasperbergman313@gmail.com>
2024-04-08 20:35:16 +0200

    Version 1.0

--- index.js
+++ index.js
@@ -1,85 +1,135 @@
-document.addEventListener("DOMContentLoaded", function () {
-  document.getElementById("btn").addEventListener("click", function (e) {
-    e.preventDefault();
+function sparaTräningspass() {
 
-    // Get the value of the "vikt_input" input field
-    var vikt = document.querySelector('input[name="vikt_input"]').value;
-    var övning = document.querySelector('select[name="övning_dropdown"]').value;
-    var storedValue = JSON.parse(localStorage.getItem(övning));
+    let övningsNamn = []
+    let vikter = []
+    let datum = new Date()
+    let children = document.getElementById("övningar").children
+    for (i = 0; i < children.length; i++) {
+      if (!children[i].children[2].checked) {
+        continue
+      }
+      övningsNamn.push(children[i].children[0].innerHTML)
+      vikter.push(children[i].children[1].value)
+      let data = JSON.parse(localStorage.getItem("övningar"))
+      data[children[i].children[0].innerHTML] = children[i].children[1].value
+      localStorage.setItem("övningar",JSON.stringify(data))
+      }
+
+
+    let träningspassJSON = {datum:{dag: datum.getDate(),månad: datum.getMonth() + 1,år: datum.getFullYear()}, övningar:{namn:övningsNamn, vikt:vikter}}
+
+
+    var storedValue = JSON.parse(localStorage.getItem("träningspass"));
     if (storedValue != null) {
-      storedValue.push(vikt);
+      storedValue.push(träningspassJSON);
     } else {
-      storedValue = new Array(vikt);
+      storedValue = new Array(träningspassJSON);
     }
     // Save the name value to localStorage
-    localStorage.setItem(övning, JSON.stringify(storedValue));
+    localStorage.setItem("träningspass", JSON.stringify(storedValue));
 
     // Log a success message (for demonstration purposes)
     console.log("Saved locally!");
-    update_table()
-  });
-});
+    uppdateraVikter()
+  }
 
-function update_table() {
-  document.getElementById("statistics_table").remove();
-  const table = document.createElement("table")
-  table.setAttribute("id","statistics_table")
-  table.setAttribute("class","statistics_table")
-  document.body.appendChild(table)
-
-  var header1 = document.createElement("th")
-  header1.appendChild(document.createTextNode("Övning 1"));
-  table.appendChild(header1)
-
-  var header2 = document.createElement("th")
-  header2.appendChild(document.createTextNode("Övning 2"));
-  table.appendChild(header2)
-  
-  var header3 = document.createElement("th")
-  header3.appendChild(document.createTextNode("Övning 3"));
-  table.appendChild(header3)
-
-
-  for (var i = 0; true; i++) {
-    var row = table.insertRow(-1);
-    var continue_loop = false;
-
-    if (localStorage.getItem("1") != null && JSON.parse(localStorage.getItem("1"))[i] != null) {
-      continue_loop = true
-      var cell = row.insertCell(-1)
-      cell.appendChild(document.createTextNode(JSON.parse(localStorage.getItem(1))[i]))
-    }
-    else {
-      var cell = row.insertCell(-1)
-    }
-    
-    if (localStorage.getItem("2") != null && JSON.parse(localStorage.getItem("2"))[i] != null) {
-      continue_loop = true
-      var cell = row.insertCell(-1)
-      cell.appendChild(document.createTextNode(JSON.parse(localStorage.getItem(2))[i]))
-    }
-    else {
-      var cell = row.insertCell(-1)
-    }
-
-    if (localStorage.getItem("3") != null && JSON.parse(localStorage.getItem("3"))[i] != null) {
-      continue_loop = true
-      var cell = row.insertCell(-1)
-      cell.appendChild(document.createTextNode(JSON.parse(localStorage.getItem(3))[i]))
-    }
-    else {
-      var cell = row.insertCell(-1)
-    }
-
-    if (!continue_loop) {
-      break
-    }
-
+function inputChange(event) {
+  if (JSON.parse(localStorage.getItem("övningar"))[event.target.parentNode.name] != event.target.value) {
+    event.target.parentNode.children[2].checked = true
   }
 }
 
 
-window.onload = update_table();
-window.onload = function() {
-  document.getElementById("datum_input").valueAsDate = new Date()
+function nyÖvning() {
+  let namn = document.getElementById("nyÖvningInput").value
+  if (localStorage.getItem("övningar")) {
+    let data = JSON.parse(localStorage.getItem("övningar"));
+    data[namn] = null
+    localStorage.setItem("övningar",JSON.stringify(data))
+  }
+  else {
+    let data = {}
+    data[namn] = null
+    localStorage.setItem("övningar",JSON.stringify(data))
+  } 
+  skrivUtÖvning(namn,null)
 }
+
+function skrivUtÖvning(namn,vikt) {
+  let övningsNod = document.createElement("div")
+  övningsNod.name = namn
+
+  let inputNod = document.createElement("input")
+  inputNod.id = 'övning-' + document.getElementById("övningar").children.length + '-input'
+  inputNod.type = "number"
+  inputNod.name = "vikt-input"
+  if (vikt) {
+  inputNod.value = vikt
+}
+  inputNod.onchange = function() {inputChange(event)}
+
+  let labelNod = document.createElement("label")
+  labelNod.for = 'övning-' + document.getElementById("övningar").children.length + '-input'
+  labelNod.innerHTML = namn
+
+  let checkboxNod = document.createElement("input")
+  checkboxNod.type = "checkbox"
+
+  övningsNod.appendChild(labelNod)
+  övningsNod.appendChild(inputNod)
+  övningsNod.appendChild(checkboxNod)
+  document.getElementById("övningar").appendChild(övningsNod)
+}
+
+
+function uppdateraVikter() {
+  taBortÖvningar()
+  if (localStorage.getItem("övningar")) {
+    console.log(localStorage.getItem("övningar"))
+    let data = JSON.parse(localStorage.getItem("övningar"))
+    for (i = 0; i < Object.keys(data).length; i++) {
+      skrivUtÖvning(Object.keys(data)[i],data[Object.keys(data)[i]])
+    }
+  }
+
+}
+
+function taBortÖvningar() {
+  var children = document.getElementById("övningar").children;
+  for (;children.length > 0;) {
+    children[0].remove()
+  }  
+}
+
+function exporteraData() {
+  let data = [JSON.stringify([localStorage.getItem("övningar"),localStorage.getItem("träningspass")])]
+  console.log(data)
+  let file = new File(data,"träningsapp-export.json")
+  const link = document.createElement('a')
+  const url = URL.createObjectURL(file)
+
+  link.href = url
+  link.download = file.name
+  document.body.appendChild(link)
+  link.click()
+
+  document.body.removeChild(link)
+  window.URL.revokeObjectURL(url)
+}
+
+function importeraData(data) {
+  var file = data.target.files[0];
+      var reader = new FileReader(); // create a FileReader
+
+    reader.onload = function(e) {
+        var contents = e.target.result; // get the file contents
+        localStorage.setItem("övningar",JSON.parse(contents)[0]); // log the contents
+        localStorage.setItem("träningspass",JSON.parse(contents)[1]); // log the contents
+        uppdateraVikter()
+    };
+
+    reader.readAsText(file); // read the file as text
+    
+}
+
+window.onload = uppdateraVikter();
